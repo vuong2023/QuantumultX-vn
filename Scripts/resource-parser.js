2c7d2ed75029a0b776ad0810bed8b309e9a6f3a3
vuong2023 <124447749+vuong2023@users.noreply.github.com>
2024-05-09 22:23:48 +0700

    Update resource-parser.js

--- Scripts/resource-parser.js
+++ Scripts/resource-parser.js
@@ -1593,7 +1593,7 @@ function Subs2QX(subs, Pudp, Ptfo, Pcert0, PTls13) {
                     }
                 } else if (type == "vless" && version<821) {
                   Perror = 1 ; // 无需反馈
-                  $notify("⚠️ Phiên bản Quantumult X của bạn không hỗ trợ nút Vless ", ⚠️ Đừng ⚠️  Chạy đến phân tích cú pháp 🤖 ️ Phản hồi",list0[i])
+                  $notify("⚠️ Phiên bản Quantumult X của bạn không hỗ trợ nút Vless ", ⚠️ Đừng ⚠️  Chạy đến phân tích cú pháp 🤖 Phản hồi",list0[i])
                 } else if (type == "vless" ) { // version 150 support vless 
                   node=VL2QX(list0[i], Pudp, Ptfo, Pcert0, PTls13)
                 } else if (QuanXK.some(NodeCheck1)) {
